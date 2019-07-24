11e2faecd3369defdecf0b7209202d68092663d7
Richard Soper <richardwebdesigner@me.com>
2019-07-23 17:23:51 -0700

    Stop apache when bringing the stack up

--- docker-up.sh
+++ docker-up.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 echo ""
+echo "Stopping Apache service"
+echo ""
+
+echo ""
 echo "Bringing the stack up"
 echo ""
+
+cd ~/docker
 
 docker-compose -f ~/docker/docker-compose.yml up -d
 
