82588458f6e6240fade4156314f17db9d244e3f9
Richard <richardwebdesigner@me.com>
2019-10-28 21:17:31 -0700

    discord-auth

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,37 +1,85 @@
-from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort, url_for, make_response
 import os
+from flask import Flask, g, session, redirect, request, url_for, jsonify
+from requests_oauthlib import OAuth2Session
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+OAUTH2_CLIENT_ID = os.environ['OAUTH2_CLIENT_ID']
+OAUTH2_CLIENT_SECRET = os.environ['OAUTH2_CLIENT_SECRET']
+OAUTH2_REDIRECT_URI = 'https://auth.richardsoper.me/callback'
+
+API_BASE_URL = os.environ.get('API_BASE_URL', 'https://discordapp.com/api')
+AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
+TOKEN_URL = API_BASE_URL + '/oauth2/token'
 
 app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = OAUTH2_CLIENT_SECRET
+
+if 'http://' in OAUTH2_REDIRECT_URI:
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
 
 
-@app.route('/', defaults={'u_path': ''})
-@app.route('/<path:u_path>')
-def home(u_path):
-    headers = request.headers
-    auth = headers.get('X-Admin-Auth')
-    if auth == 'Definitely':
-        return 'Pass', 200
-    if not session.get('logged_in'):
-        return render_template('login.html'), 403
-    else:
-        return redirect(f"https://{u_path}.richardsoper.me", code=200)
+def token_updater(token):
+    session['oauth2_token'] = token
 
 
-@app.route('/login', methods=['POST'])
-def do_admin_login():
-    u_path = request.headers.get('Referer')
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        # session['logged_in'] = True
-        # return redirect(u_path, code=302)
-        response = make_response(redirect(u_path))
-        response.headers['X-Admin-Auth'] = 'Definitely'
-        return response
-    else:
-        flash('wrong password!')
-    return home(u_path)
+def make_session(token=None, state=None, scope=None):
+    return OAuth2Session(
+        client_id=OAUTH2_CLIENT_ID,
+        token=token,
+        state=state,
+        scope=scope,
+        redirect_uri=OAUTH2_REDIRECT_URI,
+        auto_refresh_kwargs={
+            'client_id': OAUTH2_CLIENT_ID,
+            'client_secret': OAUTH2_CLIENT_SECRET,
+        },
+        auto_refresh_url=TOKEN_URL,
+        token_updater=token_updater)
 
 
-if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+@app.route('/')
+def index():
+    # scope = request.args.get(
+    #     'scope',
+    #     'identify email connections guilds guilds.join')
+    scope = request.args.get(
+        'scope',
+        'identify email')
+    discord = make_session(scope=scope.split(' '))
+    authorization_url, state = discord.authorization_url(
+        AUTHORIZATION_BASE_URL)
+    session['oauth2_state'] = state
+    return redirect(authorization_url)
+
+
+@app.route('/callback')
+def callback():
+    if request.values.get('error'):
+        return request.values['error']
+    discord = make_session(state=session.get('oauth2_state'))
+    token = discord.fetch_token(
+        TOKEN_URL,
+        client_secret=OAUTH2_CLIENT_SECRET,
+        authorization_response=request.url)
+    session['oauth2_token'] = token
+    return redirect(url_for('.me'))
+
+
+@app.route('/me')
+def me():
+    try:
+        discord = make_session(token=session.get('oauth2_token'))
+        user = discord.get(API_BASE_URL + '/users/@me').json()
+        # guilds = discord.get(API_BASE_URL + '/users/@me/guilds').json()
+        # connections = discord.get(API_BASE_URL + '/users/@me/connections').json()
+        if user['code'] == 0:
+            return redirect('/')
+    except KeyError:
+        return 'Success'
+
+
+if __name__ == '__main__':
+    app.run()
