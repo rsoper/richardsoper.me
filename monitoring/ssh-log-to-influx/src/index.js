a8b08e3b937431a47592d7e3ef688b991587cb41
Richard <richardwebdesigner@me.com>
2020-05-17 22:11:38 -0700

    manually define influxdb ip

--- monitoring/ssh-log-to-influx/src/index.js
+++ monitoring/ssh-log-to-influx/src/index.js
@@ -1,82 +1,82 @@
-import log4js from 'log4js'
-import net from 'net';
-import ngeohash from 'ngeohash'
+import log4js from "log4js";
+import net from "net";
+import ngeohash from "ngeohash";
 
-import parser from './parser'
-import doApiCall from './api';
+import parser from "./parser";
+import doApiCall from "./api";
 
 let logger = log4js.getLogger();
-logger.level = process.env.DEBUG_LEVEL || 'info';
+logger.level = process.env.DEBUG_LEVEL || "info";
 
-const Influx = require('influx');
+const Influx = require("influx");
 // InfluxDB Initialization.
 const influx = new Influx.InfluxDB({
-	host: process.env.INFLUX_URL,
-	database: process.env.INFLUX_DB,
-	username: process.env.INFLUX_USER || '',
-	password: process.env.INFLUX_PWD || '',
-	protocol: process.env.INFLUX_PROTOCOL || 'http'
+  host: "192.168.1.22:8086",
+  database: process.env.INFLUX_DB,
+  username: process.env.INFLUX_USER || "",
+  password: process.env.INFLUX_PWD || "",
+  protocol: process.env.INFLUX_PROTOCOL || "http",
 });
 
 influx.createDatabase(process.env.INFLUX_DB).catch((error) => {
-	// if the database exists or the user doesn't have sufficient privileges, this will fail
-	logger.warn(error.message);
+  // if the database exists or the user doesn't have sufficient privileges, this will fail
+  logger.warn(error.message);
 });
 
 const port = process.env.PORT || 7070;
 
 const server = net.createServer();
 
-server.on('connection', (socket) => {
+server.on("connection", (socket) => {
+  logger.info(`CONNECTED: ${socket.remoteAddress}:${socket.remotePort}`);
 
-	logger.info(`CONNECTED: ${socket.remoteAddress}:${socket.remotePort}`)
+  socket.on("data", async (data) => {
+    socket.end();
+    socket.destroy();
 
-	socket.on('data', async (data) => {
+    logger.debug("Received data", data.toString());
 
-		socket.end()
-		socket.destroy()
+    const { ip, port, username } = parser(data.toString());
+    logger.debug(`Parsed ${username} ${ip} ${port}`);
 
-		logger.debug('Received data', data.toString())
+    const ipLocation = await doApiCall(ip);
 
-		const {ip, port, username} = parser(data.toString())
-		logger.debug(`Parsed ${username} ${ip} ${port}`)
+    if (!ipLocation) {
+      logger.error("No data retrieved, cannot continue");
+      return;
+    }
 
-		const ipLocation = await doApiCall(ip);
+    const geohashed = ngeohash.encode(ipLocation.lat, ipLocation.lon);
+    logger.debug(
+      `Geohashing with lat: ${ipLocation.lat}, lon: ${ipLocation.lon}: ${geohashed}`
+    );
 
-		if(!ipLocation) {
-			logger.error('No data retrieved, cannot continue')
-			return
-		}
+    // Remove lon and lat from tags
+    const { lon, lat, ...others } = ipLocation;
 
-		const geohashed = ngeohash.encode(ipLocation.lat, ipLocation.lon);
-		logger.debug(`Geohashing with lat: ${ipLocation.lat}, lon: ${ipLocation.lon}: ${geohashed}`)
+    influx.writePoints([
+      {
+        measurement: "geossh",
+        fields: {
+          value: 1,
+        },
+        tags: {
+          geohash: geohashed,
+          username,
+          port,
+          ip,
+          location: `${ipLocation.regionName}, ${ipLocation.city}`,
+          ...others,
+        },
+      },
+    ]);
+  });
 
-		// Remove lon and lat from tags
-		const {lon, lat, ...others} = ipLocation;
-
-		influx.writePoints([
-			{
-				measurement: 'geossh',
-				fields: {
-					value: 1
-				},
-				tags: {
-					geohash: geohashed,
-					username,
-					port,
-					ip,
-					location: `${ipLocation.regionName}, ${ipLocation.city}`,
-					...others
-				}
-			}
-		]);
-	})
-
-	socket.on('close', () => {
-		logger.info(`CLOSED: ${socket.remoteAddress}:${socket.remotePort}`)
-	});
+  socket.on("close", () => {
+    logger.info(`CLOSED: ${socket.remoteAddress}:${socket.remotePort}`);
+  });
 });
 
 server.listen(port, () => {
-	logger.info(`TCP Server is running on port ${port}.`);
+  logger.info(`TCP Server is running on port ${port}.`);
 });
