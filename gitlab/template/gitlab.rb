3bc839fc4076cdf41bd86a517271f6f57c750a11
Richard <richardwebdesigner@me.com>
2021-11-27 14:59:35 -0800

    Adding registry support

--- gitlab/template/gitlab.rb
+++ gitlab/template/gitlab.rb
@@ -5,7 +5,6 @@ nginx['listen_https'] = false
 nginx['listen_port'] = 80
 
 # SMTP Settings 
-
 gitlab_rails['smtp_enable'] = true
 gitlab_rails['smtp_address'] = "smtp.zoho.com"
 gitlab_rails['smtp_port'] = 587
@@ -16,3 +15,8 @@ gitlab_rails['smtp_password'] = File.read('/run/secrets/AUTHELIA_SMTP')
 gitlab_rails['smtp_domain'] = "smtp.zoho.com"
 gitlab_rails['gitlab_email_from'] = 'noreply@richardsoper.me'
 gitlab_rails['gitlab_email_reply_to'] = 'noreply@richardsoper.me'
+
+# Registry
+registry_nginx['enable'] = true
+registry_nginx['listen_port'] = '5005'
+registry_external_url 'https://registry.richardsoper.me'
